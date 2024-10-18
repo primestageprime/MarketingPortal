5d1fe03f2fac359d7f6180951e270a389a2dcea7
Wade Allen <wade.allen@primestagetechnology.com>
2024-10-18 09:41:11 -0700

    More ScrollFade work.  Added more styling and animation, as well as some placeholder content.

--- src/lib/components/ScrollFade.svelte
+++ src/lib/components/ScrollFade.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import * as R from 'ramda';
 	import ArrowScroll from './ArrowScroll.svelte';
 
 	let sections: NodeListOf<HTMLDivElement>;
 	let scrollIndicator: HTMLDivElement;
 	let scrollContainer: HTMLDivElement;
+	let infoHeader: HTMLElement;
 
 	// Function to check if scrolled to 90% of the container
 	function handleScroll() {
@@ -30,6 +32,11 @@
 			entries.forEach((entry) => {
 				if (entry.isIntersecting) {
 					entry.target.classList.add('animate');
+					if (R.includes(entry.target.id, ['portal-start', 'contact-form'])) {
+						infoHeader.style.opacity = '0';
+					} else {
+						infoHeader.style.opacity = '1';
+					}
 				} else {
 					entry.target.classList.remove('animate');
 				}
@@ -56,52 +63,83 @@
 </script>
 
 <div bind:this={scrollContainer} class="container">
+	<header bind:this={infoHeader} class="info-header">
+		<h2 class="shiny-metallic-text finite-shine">Why partner with PrimeStage?</h2>
+	</header>
 	<div bind:this={scrollIndicator} class="scroll-indicator">
 		<ArrowScroll />
 	</div>
-	<div class="section no-interaction">
-		<div class="content-no-interaction">
-			<h1 class="shiny-metallic-text">Custom web software development</h1>
+	<div class="section no-interaction" id="portal-start">
+		<div class="content-no-interaction portal-start">
+			<h2 class="shiny-metallic-text infinite-shine">Custom web software development</h2>
 		</div>
 	</div>
 	<div class="section no-interaction">
 		<div class="content-no-interaction">
-			<div class="card content-card">Scalable and Highly Available secure web applications</div>
-		</div>
-	</div>
-	<div class="section no-interaction">
-		<div class="content-no-interaction">
-			<div class="card content-card">Mature Software Development process</div>
-		</div>
-	</div>
-	<div class="section no-interaction">
-		<div class="content-no-interaction">
-			<div class="card content-card">Client Oriented User Experience</div>
-		</div>
-	</div>
-	<!-- <div class="section no-interaction">
-		<div class="content-no-interaction">
-			<div class="content-no-interaction">
-				<div class="card">Scalable and Highly Available secure web applications</div>
+			<div class="card content-card">
+				<h3>A Proven Team of Web Experts</h3>
+				<ul>
+					<li>Decades of combined experience</li>
+					<li>Well integrated, pre built team</li>
+					<li>Eliminates the need to build or manage your own</li>
+				</ul>
 			</div>
 		</div>
 	</div>
 	<div class="section no-interaction">
 		<div class="content-no-interaction">
-			<div class="content-no-interaction">
-				<div class="card">Scalable and Highly Available secure web applications</div>
+			<div class="card content-card">
+				<h3>Mature, Efficient Development Process</h3>
+				<ul>
+					<li>
+						We have honed our software development process over years of delivering successful
+						enterprise projects
+					</li>
+					<li>Structured yet flexible methodology</li>
+					<li>Strong focus on scalability, security, and performance</li>
+					<li>Full lifecycle development support</li>
+				</ul>
 			</div>
 		</div>
-	</div> -->
-	<div class="section">
+	</div>
+	<div class="section no-interaction">
+		<div class="content-no-interaction">
+			<div class="card content-card">
+				<h3>Client Oriented User Experience</h3>
+				<ul>
+					<li>
+						<strong>User-Centered Design: </strong>We focus on understanding your users’ needs,
+						behaviors, and preferences to create intuitive, user-friendly interfaces.
+					</li>
+					<li>
+						<strong>Business Familiarity: </strong>We use the business terms, language, and visuals
+						that your users and stakeholders are already comfortable with, minimizing learning
+						curves.
+					</li>
+					<li>
+						<strong>Tailored Solutions: </strong>Whether it is custom workflows, unique business
+						logic, or specialized functionality, we build systems that align perfectly with your
+						operations.
+					</li>
+					<li>
+						<strong>Iterative Feedback: </strong>Throughout the development process, we work closely
+						with you to refine and adjust based on user feedback, ensuring the final product truly
+						meets your needs.
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="section" id="contact-form">
 		<div class="content">
-			<button class="button-primary">Click me</button>
+			<button class="button-primary">Contact Us</button>
 		</div>
 	</div>
 	<!-- Add more sections as needed -->
 </div>
 
-<style>
+<style lang="scss">
+	@import '../../scss/variables';
 	.container {
 		height: 100%; /* Adjust as needed */
 		overflow-y: auto;
@@ -113,7 +151,7 @@
 		justify-content: center;
 		align-items: center;
 		opacity: 0;
-		transition: all 1s ease;
+		transition: all 0.5s ease;
 		background-color: transparent;
 		height: 100%;
 	}
@@ -129,23 +167,47 @@
 		transition: all 1s ease;
 	}
 
-	.content-no-interaction {
-		position: fixed;
-		height: 90vh;
-		width: 100vw;
+	.info-header {
+		height: 10vh;
+		width: 100%;
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		position: fixed;
 		top: 10vh;
+		left: 0;
+		opacity: 0;
+		transition: all 1s ease;
+		padding: 0.5rem;
+	}
+
+	.content-no-interaction {
+		position: fixed;
+		height: 80vh;
+		width: 100vw;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		top: 20vh;
 		left: 0;
 		pointer-events: none; /* Allows interaction with underlying elements */
 		padding: 1rem;
 	}
 
+	.content-no-interaction.portal-start {
+		height: 90vh;
+		top: 10vh;
+	}
+
 	.content-card {
-		height: 100%;
-		width: 100%;
 		background-color: transparent;
+		height: 100%;
+		width: 70%;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
 	}
 
 	:global(.section.animate) {
@@ -157,9 +219,8 @@
 		font-size: 4rem;
 		font-weight: bold;
 		background: linear-gradient(135deg, #4667ae, #7792c6, #c0d2fb, #7792c6, #4667ae, #224795);
-		background-size: 300%; /* Increased size for a smoother transition */
+		background-size: 400%; /* Increased size for a smoother transition */
 		color: transparent;
-		animation: shine 6s infinite ease-in-out;
 
 		/* Vendor prefixes for compatibility */
 		-webkit-background-clip: text; /* Safari, Chrome */
@@ -167,6 +228,17 @@
 		-ms-background-clip: text; /* Internet Explorer */
 		-o-background-clip: text; /* Older Opera */
 		background-clip: text; /* Standard */
+	}
+
+	.infinite-shine {
+		animation: shine 6s infinite ease-in-out;
+	}
+
+	.shiny-metallic-text.finite-shine {
+		width: 80%;
+		animation: shine 3s linear 1;
+		text-align: center;
+		font-size: 3rem;
 	}
 
 	@keyframes shine {
@@ -198,8 +270,52 @@
 
 	@media (orientation: landscape) {
 		.content-no-interaction {
-			height: 90vh;
-			top: 10vh;
+			height: 80vh;
+			top: 20vh;
+		}
+	}
+
+	// Tablet (768px and below)
+	@media #{$tablet-portrait} {
+		html {
+			font-size: 15px; // Slightly smaller font size
+		}
+
+		.header,
+		.footer {
+			padding: 0.8rem 1.5rem; // Reduce padding
+			font-size: 0.9rem; // Smaller font size for header/footer
+		}
+		.button-primary,
+		.button-secondary,
+		.button-secondary-white {
+			padding: 0.65rem 1.25rem; // Slightly smaller padding
+			font-size: 0.9rem; // Slightly smaller font size
+		}
+	}
+
+	// Phone (480px and below)
+	@media #{$small-screen} {
+		.shiny-metallic-text {
+			font-size: 2rem;
+		}
+	}
+	// Phone (599px and below)
+	@media #{$phone-portrait} {
+		.shiny-metallic-text {
+			font-size: 2rem;
+		}
+	}
+
+	@media #{$tablet-landscape} {
+		.shiny-metallic-text {
+			font-size: 2rem;
+		}
+	}
+
+	@media #{$phone-landscape} {
+		.shiny-metallic-text {
+			font-size: 2rem;
 		}
 	}
 </style>
