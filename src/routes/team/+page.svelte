0422b69f5bd344ef2df38748eacb201589e085b9
Wade Allen <wade.allen@primestagetechnology.com>
2024-11-12 12:27:50 -0800

    Added about me for Wade.

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import EmployeeInfo from '$lib/components/layout/EmployeeInfo.svelte';
 	import { slide } from 'svelte/transition';
+	import { wadeAboutMe } from './constants';
 
 	const ipsum =
 		'Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent rutrum sit consequat commodo dapibus imperdiet parturient faucibus. Adipiscing nibh enim adipiscing primis vivamus, dui ultricies ornare. Donec maecenas ultrices id tortor lobortis platea? Velit dapibus sit consequat imperdiet blandit varius curabitur integer molestie. Quisque velit ridiculus duis senectus etiam netus. Feugiat eros aptent natoque phasellus ante eu tempor facilisis. Risus hendrerit justo natoque metus tempus vehicula. Blandit magna tortor luctus faucibus, id potenti himenaeos nibh curae. Platea fermentum at feugiat vivamus posuere condimentum. Ut ligula vulputate placerat nascetur; quis curabitur cras ad. Donec maecenas natoque, semper semper urna elementum eu. Congue vulputate tortor sem curae donec. Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent rutrum sit consequat commodo dapibus imperdiet parturient faucibus. Adipiscing nibh enim adipiscing primis vivamus, dui ultricies ornare. Donec maecenas ultrices id tortor lobortis platea? Velit dapibus sit consequat imperdiet blandit varius curabitur integer molestie. Quisque velit ridiculus duis senectus etiam netus. Feugiat eros aptent natoque phasellus ante eu tempor facilisis.';
@@ -11,7 +12,7 @@
 		<EmployeeInfo fullName="Chunk The Squirrel" imageSrc={'/images/Chunk.png'} aboutMe={ipsum} />
 	</div>
 	<div class="employee-info-section d-fr">
-		<EmployeeInfo fullName="Roo Kanagadoo" imageSrc={'/images/Roo.png'} aboutMe={ipsum} />
+		<EmployeeInfo fullName="Wade Allen" imageSrc={'/images/Wade.png'} aboutMe={wadeAboutMe} />
 	</div>
 	<div class="employee-info-section d-fr">
 		<EmployeeInfo fullName="Fran" imageSrc={'/images/Fran.png'} aboutMe={ipsum} />
